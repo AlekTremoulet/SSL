25c233e3dfd6ceeba39e5ca8da45c5cb321ca343
AlekTremoulet <atremoulet@frba.utn.edu.ar>
2025-04-23 15:34:15 -0300

    v 0.5

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
 int main(void){
-    FILE *salida;
-    char nombre[20] = "Hola Alek Tremoulet!";
-    printf(nombre);
-   if (( salida = fopen ("output.txt","w"))){
-    fwrite(nombre,sizeof(char),20,salida);
-    }
-    else 
-        return EXIT_FAILURE;
-    fclose(salida);
+    printf("Hola Alek Tremoulet!");
 return EXIT_SUCCESS;
 }
